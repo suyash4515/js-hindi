f266105a4fa7bc6f93fe15526305c8c1a385e429
suyash4515 <94191192+suyash4515@users.noreply.github.com>
2023-10-08 18:29:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node JS  not browser
+
+console.log(3+2) // code readability
+
+
+let name="suyash"
+let age =18
+let issLoggedIn = true
+
+
+
+// number => 2 to power 33 (range)
+//bigint
+//string =>""
+//bollean =>true or false
+//null =>stand alone value
+// undefined =>
+// symbol =>unique
+
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof null); // object
