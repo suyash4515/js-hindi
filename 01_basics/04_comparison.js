1bfe5a69c3f8bb568161d4a5919ec6642ce62d71
suyash4515 <94191192+suyash4515@users.noreply.github.com>
2023-10-09 17:28:43 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 < 3);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0); // rules for comparison and equaling is diifferent
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined > 0); // rules for comparison and equaling is diifferent
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+// === datatype has to be same, 
+
+console.log("2" === 2);
