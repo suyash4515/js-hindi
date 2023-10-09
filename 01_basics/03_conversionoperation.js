532b9b46711ef2115483e5dfb4c7df8eb9554f82
suyash4515 <94191192+suyash4515@users.noreply.github.com>
2023-10-09 14:05:27 +0000

    conversion discussed

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,81 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2**2);
+// console.log(2**3);
+
+// let str1 = "suyash"
+// let str2 = "dhanaji"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(3 + 4 * 4 % 2);
+
+
+
+console.log(+true);
+console.log(+"");
+
+
+
+// let num1 , num2 , num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
