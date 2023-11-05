4bc93b30d003cf4c2f9d31e2724dc5c1d1f1479e
suyash4515 <94191192+suyash4515@users.noreply.github.com>
2023-11-05 09:03:50 +0000

    datatypes

--- 01_basics/datatypesummary.js
+++ 01_basics/datatypesummary.js
@@ -9,7 +9,7 @@
 
 // Array , Objects , Fumctions
 
-const heroes = ["suyash"  , "ashish" , "vedant"];
+/* const heroes = ["suyash"  , "ashish" , "vedant"];
 
 let myboj={
         name:suyash,
@@ -22,3 +22,29 @@ const myfunction = function(){
 
     console.log("suyash"); 
 }
+
+*/
+// ********************************************************
+
+// stack(primitive) ,, heap(non primitive) =>memory
+
+let myYoutubename = "suyashindalkar"
+
+let anothername = myYoutubename
+anothername = "abhijet"
+console.log(anothername);
+console.log(myYoutubename);
+
+let user1 = {
+
+    email:"suyash@gmail.com",
+    upi:"suyash@axl"
+
+}
+
+let user2 = user1
+
+user2.email = "suyashinda@gmail.com"
+
+ console.log(user1.email);
+ console.log(user2.email);
