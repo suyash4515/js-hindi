d49fbaffa123680cf7a9e4e75985df1a17e205d1
suyash4515 <94191192+suyash4515@users.noreply.github.com>
2023-11-05 05:51:24 +0000

    datatypesummary

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -79,3 +79,4 @@ const b = ++a;
 
 console.log(`a:${a}, b:${b}`);
 // Expected output: "a:4, b:4"
+ 
---
+++ datatypesummary.js
@@ -0,0 +1,24 @@
+// primitive =>call by value
+
+// 7 types : string  , Number , Bollean , null ,undefined , symbbol , BigInt
+
+
+//
+
+// Reference (Non Primitive)
+
+// Array , Objects , Fumctions
+
+const heroes = ["suyash"  , "ashish" , "vedant"];
+
+let myboj={
+        name:suyash,
+        age:24,
+
+}
+
+
+const myfunction = function(){
+
+    console.log("suyash"); 
+}
