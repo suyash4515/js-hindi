3454a3b976db260ac86e12c72fac81397a1af94a
suyash4515 <94191192+suyash4515@users.noreply.github.com>
2023-11-09 14:59:10 +0000

    Maths

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,38 @@
+/*const score = 400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+
+const othernumber = 11123.896
+
+console.log(othernumber.toPrecision(4));
+
+const number1 = 10000000
+console.log(number1.toLocaleString('en-IN'));
+*/
+
+// **********************Maths**************
+
+
+//console.log(Math);
+//console.log(Math.abs(99-4));
+//console.log(Math.round(4.3));
+//console.log(Math.ceil(4.3));
+//console.log(Math.floor(4.3));
+//console.log(Math.min(4,3,6));
+//console.log(Math.max(4,3,6));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min ))  => IMP
+
+
